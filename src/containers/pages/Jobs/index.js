6094fe93fc6eccadaa4582b1def0123ebf7ec929
konovalov i <ivan.konovalov@noveogroup.com>
2020-01-31 00:11:58 +0700

    added styles to Jobs page

--- src/containers/pages/Jobs/index.js
+++ src/containers/pages/Jobs/index.js
@@ -2,21 +2,24 @@ import React, { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { requestJobsIfNeed } from "actions/jobs"
 import { TEST_ORGANIZATION_REF } from "constants/jobs"
-import { Text } from "welcome-ui"
-import { selectJobs } from "selectors/jobs"
+import { Text, Box } from "welcome-ui"
+import { selectJobsCollection } from "selectors/jobs"
 import { Loader } from "components/Loader"
+import { JobsList } from "components/JobsList"
 
 export const Jobs = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(requestJobsIfNeed({ organizationRef: TEST_ORGANIZATION_REF }))
+    dispatch(requestJobsIfNeed({ organization: TEST_ORGANIZATION_REF }))
   }, [dispatch])
-  const jobs = useSelector(selectJobs)
+  const jobs = useSelector(selectJobsCollection)
 
   return (
-    <div>
-      <Text variant="h3">Our offers</Text>
-      {!jobs.length && <Loader />}
-    </div>
+    <Box backgroundColor="light.200" p="20px 80px" borderRadius="lg" mt={80}>
+      <Box display="flex" justifyContent="center">
+        <Text variant="h3">Our offers</Text>
+      </Box>
+      {jobs.length ? <JobsList jobs={jobs} /> : <Loader />}
+    </Box>
   )
 }
