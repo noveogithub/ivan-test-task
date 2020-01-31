92e6d3ad4a7a1c7c2609c82325c43ebbbc640bd6
konovalov i <ivan.konovalov@noveogroup.com>
2020-01-31 23:53:27 +0700

    added jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
+/**
+ * this config is just for local jest running (e.g. in pre commit hooks)
+ * or for other cases when react scripts testing does not fit
+ * react-scripts test doesn't need this config
+ */
 module.exports = {
   roots: ["<rootDir>/src"],
   moduleDirectories: ["node_modules", "src"],
