f80653aa307c8813424347ba34eb21ce8be9ec5e
konovalov i <ivan.konovalov@noveogroup.com>
2020-01-31 21:04:22 +0700

    yarn lock and dependencies commited

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React from "react"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import { JOB_PREVIEW_ROUTE, JOBS_ROUTE } from "constants/routes"
+import { Job } from "containers/pages/Job"
 import { Jobs } from "containers/pages/Jobs"
 import { ThemeProvider } from "@xstyled/styled-components"
 import { createTheme } from "@welcome-ui/core"
-import { Box, Text } from "welcome-ui"
+import { Box } from "@welcome-ui/box"
 
 const theme = createTheme({})
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Box justifyContent="center" display="flex" backgroundColor="light.200">
-        <Box m="4">
-          <Text variant="h2">Jobs app</Text>
+      <Box justifyContent="center" display="flex" backgroundColor="light.700">
+        <Box width={{ sm: 1, md: 0.5 }} minHeight="100vh">
           <BrowserRouter>
             <Switch>
               <Route path={JOBS_ROUTE} exact component={Jobs} />
-              <Route
-                path={JOB_PREVIEW_ROUTE}
-                exact
-                component={() => <div>exact job</div>}
-              />
+              <Route path={JOB_PREVIEW_ROUTE} exact component={Job} />
             </Switch>
           </BrowserRouter>
         </Box>
